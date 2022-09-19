6624e5e5f9786d4fffcf242df2bd9fe91058c600
Tessa Thornberry <tthornberryclass@gmail.com>
2022-09-19 11:01:54 -0700

    pass GroceryList React-Components before Hooks

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // TODO
+const GroceryList = () => (
+    <div>
+        <h3>Grocery List</h3>
+        <GroceryListItem items={['Coffee', 'Coffee filters']}/>
+    </div>
+);
+
+const GroceryListItem = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+  </ul>
+)
+
+const Coffee = () => (
+    <li>Coffee</li>
+);
+
+
+const CoffeeFilters = () => (
+    <li>Coffee filters</li>
+);
+
+ReactDOM.render(<GroceryList/>, document.getElementById("app"));
